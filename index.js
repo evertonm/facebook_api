b4b58b9c07dab662861596fd6b5c8de7645ae1fd
Sandro Ireno Martins Junior <sandro.ireno@gmail.com>
2021-07-27 19:31:36 -0300

    inicio ajuste busca token automaticamente

--- index.js
+++ index.js
@@ -49,91 +49,91 @@ fs.writeFile('./access_token.json', JSON.stringify(arquivo), function (err) {
 });
 
 
-// const fields = [
-//   'campaign_id',
-//   'account_id',
-//   'account_name',
-//   'account_currency',
-//   'inline_link_clicks',
-//   'impressions',
-//   'clicks',
-//   'spend',
-//   'cpm',
-//   'frequency',
-//   'cpp',
-//   'ctr',
-//   'reach',
-//   'cost_per_conversion',
-//   'actions',
-//   'action_values',
-//   'location'
-// ];
+const fields = [
+  'campaign_id',
+  'account_id',
+  'account_name',
+  'account_currency',
+  'inline_link_clicks',
+  'impressions',
+  'clicks',
+  'spend',
+  'cpm',
+  'frequency',
+  'cpp',
+  'ctr',
+  'reach',
+  'cost_per_conversion',
+  'actions',
+  'action_values',
+  'location'
+];
 
-// const params = {
-//   'breakdowns': 'region',
-//   'time_range': { 'since': '2021-07-01', 'until': '2021-07-30' },
-// };
+const params = {
+  'breakdowns': 'region',
+  'time_range': { 'since': '2021-07-01', 'until': '2021-07-30' },
+};
 
-// new AdAccount(ad_account_id).getInsights(
-//   fields,
-//   params
-// ).then((response) => {
-//   response.forEach((obj) => {
-//     console.log(obj._data)
-//   });
-// });
+new AdAccount(ad_account_id).getInsights(
+  fields,
+  params
+).then((response) => {
+  response.forEach((obj) => {
+    console.log(obj._data)
+  });
+});
 
-// //Sample usage momentjs
-// const date = moment().format();
-// console.log(date);
+//Sample usage momentjs
+const date = moment().format();
+console.log(date);
 
-// writeFile('nomura');
+writeFile('nomura');
 
-// function writeFile(client) {
+function writeFile(client) {
 
-//   const path = `${client}.json`;
-//   let dateStart = '2021-07-01';
-//   let dataToInsert;
-//   fs.access(path, fs.F_OK, (err) => {
-//     if (err) { //Arquivo não existe
-//       console.log('não existe');
+  const path = `${client}.json`;
+  let dateStart = '2021-07-01';
+  let dataToInsert;
+  fs.access(path, fs.F_OK, (err) => {
+    if (err) { //Arquivo não existe
+      console.log('não existe');
 
-//       //Criar lógica para pegar da data: 2021-07-01 até hoje
+      //Criar lógica para pegar da data: 2021-07-01 até hoje
 
-//       dataToInsert = [];
+      dataToInsert = [];
 
-//       dataToInsert.push({
-//         id: dataToInsert.length + 1,
-//         date_start: '2021-07-03'
-//       })
+      dataToInsert.push({
+        id: dataToInsert.length + 1,
+        date_start: '2021-07-03'
+      })
 
 
-//     } else {
-//       const fileData = JSON.parse(fs.readFileSync(path));
+    } else {
+      const fileData = JSON.parse(fs.readFileSync(path));
 
-//       if (fileData[fileData.length - 1].date_start) {
-//         dateStart = fileData[fileData.length - 1].date_start;
-//       }
-//       dataToInsert = fileData;   
+      if (fileData[fileData.length - 1].date_start) {
+        dateStart = fileData[fileData.length - 1].date_start;
+      }
+      dataToInsert = fileData;   
       
-//       dataToInsert.push({
-//         id: dataToInsert.length + 1,
-//         date_start: '2021-07-04'
-//       })
+      dataToInsert.push({
+        id: dataToInsert.length + 1,
+        date_start: '2021-07-04'
+      })
     
-//       //Criar lógica para pegar da última data inserida até hoje
+      //Criar lógica para pegar da última data inserida até hoje
 
-//     }
+    }
 
 
-//     //inserir dado no json
-//     if(dataToInsert) {
-//       fs.writeFile(path, JSON.stringify(dataToInsert), function (err) {
-//         if (err) throw err;
-//         console.log('Thanks, It\'s saved to the file!');
-//       });
-//     }
+    //inserir dado no json
+    if(dataToInsert) {
+      fs.writeFile(path, JSON.stringify(dataToInsert), function (err) {
+        if (err) throw err;
+        console.log('Thanks, It\'s saved to the file!');
+      });
+    }
 
-//   })
-// }
+  })
+}
 
